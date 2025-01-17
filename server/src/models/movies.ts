60ab6dc6c524c6b930a6c691976625f78a47d2f7
Harry Oyarvide <poyarvide87@yahoo.com>
2025-01-17 12:12:15 -0500

    // Optional attributes when creating a new Joke entry

--- server/src/models/movies.ts
+++ server/src/models/movies.ts
@@ -8,6 +8,19 @@ interface MovieAttributes {
     rating: number;
 }
 
+// Optional attributes when creating a new Movie entry
+interface MovieCreationAttributes extends Optional<MovieAttributes, 'id'> {}
+
+export class Movie extends Model<MovieAttributes, MovieCreationAttributes> implements MovieAttributes {
+    public id!: number;
+    public title!: string;
+    public genre!: string;
+    public rating!: number;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
 export function MovieStream(sequelize: Sequelize): typeof Movie {
     MovieStream.init(
         {
